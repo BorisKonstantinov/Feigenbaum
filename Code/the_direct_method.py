7e8397962c5310e3b599a3b00cae93e184a8176d
Aphus <b-ivanov@outlook.com>
2024-09-21 23:07:20 +0100

    modified:   Code/the_direct_method.py

--- Code/the_direct_method.py
+++ Code/the_direct_method.py
@@ -4,11 +4,12 @@ Created on Tue Apr 21 15:54:29 2020
 Based on Briggs, 1991: https://www.jstor.org/stable/2938684
 """
 
-def feigenbaum(max_it=25, max_it_k=100, mu1= 1, mu2= 0, f=4.92, p=0, dp=0):
-    
-    for i in range(2, max_it + 1):    
-        
-        mu = mu1 + (mu1 - mu2) / f   * ( 12222222 + 223 + 34*(123123*1231231232*(1232132222222)))
+
+def feigenbaum(max_it=25, max_it_k=100, mu1=1, mu2=0, f=4.92, p=0, dp=0):
+
+    for i in range(2, max_it + 1):
+
+        mu = mu1 + (mu1 - mu2) / f
         for k in range(1, max_it_k + 1):
             for n in range(1, (1 << i) + 1):
                 dp = 1.0 - 2.0 * dp * p
