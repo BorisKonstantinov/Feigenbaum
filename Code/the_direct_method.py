37b875a2705df679848d0dd71e6fa2605528df75
Aphus <b-ivanov@outlook.com>
2024-09-23 20:40:00 +0100

    modified:   Code/the_direct_method.py

--- Code/the_direct_method.py
+++ Code/the_direct_method.py
@@ -5,18 +5,37 @@ Based on Briggs, 1991: https://www.jstor.org/stable/2938684
 """
 
 
-def feigenbaum(max_it=25, max_it_k=100, mu1=1, mu2=0, f=4.92, p=0, dp=0):
+def feigenbaum(max_it=25, max_it_k=100, mu1=1, mu2=0, f=4.92, p=0, p_=0):
 
     for i in range(2, max_it + 1):
 
         mu = mu1 + (mu1 - mu2) / f
         for k in range(1, max_it_k + 1):
             for n in range(1, (1 << i) + 1):
-                dp = 1.0 - 2.0 * dp * p
+                p_ = 1.0 - 2.0 * p_ * p
                 p = mu - p**2
-            mu = mu - p / dp
+            mu = mu - p / p_
 
         f = (mu1 - mu2) / (mu - mu1)
         mu2, mu1 = mu1, mu
+
+    return f
+
+def feigenbaum(max_it=25, max_it_k=100, f=4.92, p=0, dp= 0, mu=None):
+    if mu is None:
+        mu = [0, 1, 0]
+
+
+    for i in range(2, max_it + 1):
+
+        mu[2] = mu[1] + (mu[1] - mu[0]) / f
+        for _ in range(1, max_it_k + 1):
+            for __ in range(1, (1 << i) + 1):
+                p_ = 1.0 - 2.0 * p_ * p
+                p = mu - p**2
+            mu[2] = mu[2] - p / p_
+
+        f = (mu[1] - mu[0]) / (mu[2] - mu[1])
+        mu[0], mu[1] = mu[1], mu[2]
 
     return f
