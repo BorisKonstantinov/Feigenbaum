cfab69a2e0da9cf7cffdc106f49c9a391ccab6f5
Boris Ivanov <b-ivanov@outlook.com>
2024-10-01 14:34:07 +0000

    modified:   Code/lyapunov_multiprocessing.py

--- Code/lyapunov_multiprocessing.py
+++ Code/lyapunov_multiprocessing.py
@@ -5,17 +5,15 @@ Created on Fri Apr 10 22:01:04 2020
 @author: 967869@swansea.ac.uk
 """
 
-import decimal as d
 import multiprocessing as mp
 
-import numpy as np
 from matplotlib import pyplot
 
 
-def lyapunov(rate):
+def lyapunov(rate, dx, N):
     """Returns Lyapunov exponent for rate over N gen."""
     function = 0.2
-    d_function = function + DX
+    d_function = function + dx
     sigma = 0
 
     for _ in range(N):
@@ -23,28 +21,28 @@ def lyapunov(rate):
         d_function = rate * d_function * (1 - d_function)
 
         diff = abs(d_function - function)
-        if diff < epsilon:
+        if diff == 0:
             break
 
-        sigma += math.log(diff / DX)
+        sigma += math.log(diff / dx)
 
     sigma = sigma / N  # (i + 2)
     return sigma
 
 
-def loop(start, stop, X, Y):
+def loop(start, stop, x, y, resolution, dx, N):
     """Runs Lyapunov over a range of rates"""
     for i in range(start, stop):
-        X[i - start] = i / RESOLUTION
-        Y[i - start] = lyapunov(i / RESOLUTION)
+        x[i - start] = i / resolution
+        y[i - start] = lyapunov(i / resolution, dx, N)
 
 
 def run(
-    start_range=3000, end_range=4000, num_of_cpu=10, RESOLUTION=1000, DX=1e-8, N=1e4
+    start_range=3000, end_range=4000, num_of_cpu=10, resolution=1000, dx=1e-8, N=1e4
 ):
     """Runs the lyapunov function"""
-    X = mp.Array("d", RESOLUTION)
-    Y = mp.Array("d", RESOLUTION)
+    x = mp.Array("d", resolution)
+    y = mp.Array("d", resolution)
 
     processes = []
 
@@ -52,21 +50,21 @@ def run(
 
     if (end_range - start_range) % num_of_cpu != 0:
         leftover = (end_range - start_range) % num_of_cpu
-        p = mp.Process(target=loop, args=(start_range, step + leftover, X, Y))
+        p = mp.Process(
+            target=loop, args=(start_range, step + leftover, x, y, resolution, dx, N)
+        )
         processes.append(p)
         p.start()
         start_range += leftover + step
 
     for i in range(start_range, end_range, step):
-        p = mp.Process(target=loop, args=(i, i + step, X, Y))
+        p = mp.Process(target=loop, args=(i, i + step, x, y))
         processes.append(p)
         p.start()
 
     for p in processes:
         p.join()
 
-    ZERO = np.zeros(RESOLUTION)
-
     pyplot.axhline(y=0)
-    pyplot.plot(X, Y)
+    pyplot.plot(x, y)
     pyplot.show()
