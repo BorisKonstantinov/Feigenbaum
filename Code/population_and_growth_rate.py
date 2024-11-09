25e6486bc73868d77060c0b6485294731e4b5530
Boris Konstantinov <b-ivanov@outlook.com>
2024-11-09 00:15:47 +0000

    deleted:    Code/Project.py 	modified:   Code/population_and_growth_rate.py

--- Code/population_and_growth_rate.py
+++ Code/population_and_growth_rate.py
@@ -1,6 +1,14 @@
 """
-instructions
+The map x = µx(1 - x) can be seen to be stable between
+one or many population sizes up to a certain point close to 3.57,
+where the population size becomes chaotic. The graph produced
+by this code shows this behaviour.
 
+Found in Chapter 3 Towards an understanding of chaos
+
+This code has been modified in 2024 for the purposes of improving
+readability and functionality.
+Original version can be found in the appendix.
 """
 import numpy as np
 from matplotlib import pyplot
@@ -21,24 +29,30 @@ class PopulationSimulator:
         self.Y = np.zeros(array_length)
 
     def Map(self, µ, population):
+        """ The map x = µx(1 - x)"""
         return µ * population * (1 - population)
 
     def Cycle(self):
+        """ After a number of initial itterations the system begins
+        its oscillation between stable values (where aplicable), those values are then recorded."""
+        index = 0
         for µ in np.arange(0, 4, self.resolution_mu):
-            self.X[int(µ * (1 / self.resolution_mu)) : int(µ * (1 / self.resolution_mu) + self.member_count)] = µ
+            # self.X[int(µ * (1 / self.resolution_mu)) : int(µ * (1 / self.resolution_mu) + self.member_count)] = µ
             population = self.population
             for _ in range(5000):
                 """ Itterate to approach equilibrium.""" 
                 population = self.Map(µ, population)
-            for i in range(self.member_count):
+            for _ in range(self.member_count):
                 population = self.Map(µ, population)
-                self.Y[i] = population
+                self.X[index] = µ
+                self.Y[index] = population
+                index += 1
 
 
     def plot(self):
         """ Plots the values of X and Y"""
         pyplot.figure(dpi=480)
-        pyplot.plot(self.X, self.Y, ".", markersize=0.1)
+        pyplot.plot(self.X, self.Y, ".", markersize=0.01)
         pyplot.xlabel("Growth rate - µ")
         pyplot.ylabel("Population")
         pyplot.title("Population supported at different growth rates")
@@ -47,6 +61,7 @@ class PopulationSimulator:
         pyplot.show()
 
     def run(self):
+        """ Runs the simulation"""
         self.Cycle()
         self.plot()
 
