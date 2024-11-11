46e8add5764b1552d7777bd243896f32d21adfd2
Boris Konstantinov <b-ivanov@outlook.com>
2024-11-11 17:08:57 +0000

    Removed project function

--- Code/experiments.py
+++ Code/experiments.py
@@ -14,19 +14,6 @@ class PopulationSimulator:
     def Law(self, population, rate):
         return rate * population * (1 - population)
 
-
-    def project(self):
-        X = np.zeros(self.resolution)
-        Y = np.zeros(self.resolution)
-        population = self.population
-        for rate in np.arange(0, 4, 0.001):
-            for _ in range(2000):
-                population = self.Law(population, rate)
-            for i in range(self.resolution):
-                X[i] = rate
-                Y[i] = population = self.Law(population, rate)
-        return X, Y
-
     def cobweb(self):
         X = np.zeros(self.resolution)
         Y = np.zeros(self.resolution)
@@ -44,9 +31,6 @@ class PopulationSimulator:
         X[-2] = population
         return X, Y
 
-    def transitivity(self):
-        return None
-
     def plot(self, X, Y, args, xlabel, ylabel, title):
         pyplot.plot(X, Y, args)
         pyplot.xlabel(xlabel)
@@ -60,23 +44,5 @@ class PopulationSimulator:
         X, Y = self.project()
         self.plot(X, Y, ".", "x", "f(x)", "max_value experiments.py")
 
-
 simulator = PopulationSimulator()
 simulator.run()
-
-
-# -----------------------------------------------------------------------------
-def Law(rate):
-    population = 0.2
-    list = []
-
-    for i in range(2000000):
-        population = rate * population * (1 - population)
-
-    for i in range(50000):
-        population = rate * population * (1 - population)
-        rounded = round(population, 5)
-        if rounded not in list:
-            list.append(rounded)
-
-    return len(list)
